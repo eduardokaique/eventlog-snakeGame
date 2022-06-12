6c5f826e22507a44c8e46e88944aecd8cff81b73
Eduardo Kaique <dudukaique@hotmail.com>
2022-06-12 15:23:54 -0300

    ajuste run start

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
-import { serverHttp } from "./app";
+import "dotenv/config"
+import express, { response } from "express";
+import http from "http";
+import cors from "cors";
+import { router } from "./routes";
+
+const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+app.use(express.json());
+app.use(router);
+export { serverHttp };
+
 const port  = process.env.PORT || 3000;
 serverHttp.listen(port, () => console.log("🚀 Server is run on PORT " + port));
